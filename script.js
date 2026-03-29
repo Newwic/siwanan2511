c042b1c6f0d7219e76dc921e7909a8c13c8561a1
Newwic <newtvthnew@gmail.com>
2026-03-29 15:24:58 +0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function openImg(src) {
+    document.getElementById("popup").style.display = "block";
+    document.getElementById("popupImg").src = src;
+}
+
+function closeImg() {
+    document.getElementById("popup").style.display = "none";
+}
